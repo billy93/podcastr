270d93a20c06d6ceea76487d887c7b4710dd0b63
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-06-21 23:27:19 +0700

    complete

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://novel-magpie-96.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
